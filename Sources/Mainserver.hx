95fcf1caf7ee526b4466216f0387c8db111248e8
amiani <amianijohns@gmail.com>
2019-03-08 19:22:04 -0500

    added first attempt at headless mode

---
+++ Sources/Mainserver.hx
@@ -0,0 +1,31 @@
+package;
+
+import kha.Assets;
+
+class Mainserver {
+  static var spacechase : Spacechase;
+	static var TIMESTEP = 1/60;
+
+	public static function main() {
+		Assets.loadEverything(() -> {
+			// Just loading everything is ok for small projects
+			spacechase = new Spacechase(1024, 768);
+			var accumulator = 0.;
+			var now = Sys.time()*1000;
+			while (true) {
+				var newTime = Sys.time()*1000;
+				var frameTime = newTime - now;
+				now = newTime;
+				accumulator += frameTime;
+
+				while (accumulator >= TIMESTEP) {
+					trace('about to update');
+					spacechase.update();
+					accumulator -= TIMESTEP;
+				}
+			}
+			// Avoid passing update/render directly,
+			// so replacing them via code injection works
+		});
+	}
+}
